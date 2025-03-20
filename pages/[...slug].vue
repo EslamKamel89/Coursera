e9937f70723b94487caca98440502595287a360d
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-20 16:07:24 +0200

    add 404 error page and refactor user profile route parameters

---
+++ pages/[...slug].vue
@@ -0,0 +1,20 @@
+<template>
+  <h2 class="text-3xl font-bold font-mono mb-2 uppercase">Error 404</h2>
+  <div class="" style="margin-bottom: 10px">Page Not Found</div>
+  <div></div>
+  <div>
+    <div v-if="slugs.length > 0">
+      <span>Path (</span>
+      <span v-for="slug in slugs" :key="slug" class="text-lg font-bold">
+        {{ slug }}/
+      </span>
+      <span>) Not Found </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const slugs = useRoute().params.slug as string[];
+</script>
+
+<style scoped></style>
--- pages/users/[type]/group-[id].vue
+++ pages/users/[type]/group-[id].vue
@@ -1,10 +1,8 @@
 <template>
   <h2 class="text-3xl font-bold font-mono mb-2 uppercase">
-    Your profile {{ route.params.type }}, Your ID is {{ route.params.id }}
+    Your profile {{ $route.params.type }}, Your ID is {{ $route.params.id }}
   </h2>
   <Info />
 </template>
 
-<script setup lang="ts">
-const route = useRoute();
-</script>
+<script setup lang="ts"></script>
