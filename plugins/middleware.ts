98baab38e8e544c50a3fc6d56a0d911cc50a8fdc
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-21 13:37:40 +0200

    add middleware to restrict access to database and specific country routes

--- plugins/middleware.ts
+++ plugins/middleware.ts
@@ -5,6 +5,14 @@ export default defineNuxtPlugin((nuxtApp) => {
       if (to.path == "/database") {
         return abortNavigation("You cant visit the database page");
       }
+      if (
+        (to.params.country as string | undefined)?.toLowerCase().trim() ==
+          "israel" ||
+        (to.query.country as string | undefined)?.toLowerCase().trim() ==
+          "israel"
+      ) {
+        return abortNavigation("Fuck Israel");
+      }
       console.log("Global Dynamic Middleware in the plugins directory");
     },
     { global: true }
