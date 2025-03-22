a7685ca2f4fbc7f658d6d76f63fbe105847a12fe
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-22 17:31:44 +0200

    add Nuxt.js component with info toggle and new images

---
+++ components/Nuxt.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="text-sm leading-loose border rounded-xl bg-gray-100 shadow-lg px-4 py-2 mt-2"
+  >
+    Nuxt.js is a powerful framework built on top of Vue.js.. It simplifies the
+    development of server-side rendered (SSR) applications, static websites, and
+    Single Page Applications (SPAs). Its main goal is to provide a seamless and
+    structured development experience, particularly for creating performant,
+    SEO-friendly applications with minimal configuration.
+  </div>
+</template>
+
+<script setup lang="ts">
+console.log("Nuxt js component is loaded");
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,26 @@
 <template>
   <h2 class="text-3xl font-bold font-mono mb-2 uppercase">home</h2>
-  <Info />
+  <button
+    class="bg-blue-500 text-white px-3 py-2 rounded-xl"
+    @click="toggleInfo"
+  >
+    {{ showInfo ? "Hide" : "Show" }} Info
+  </button>
+  <template v-if="showInfo">
+    <img
+      src="/assets/images/programming2.png"
+      class="rounded-lg my-2 shadow-lg"
+    />
+    <LazyNuxt />
+    <img src="/images/programming.png" class="rounded-lg my-2 shadow-lg" />
+  </template>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const showInfo = ref(false);
+const toggleInfo = () => {
+  showInfo.value = !showInfo.value;
+};
+</script>
 
 <style scoped></style>
